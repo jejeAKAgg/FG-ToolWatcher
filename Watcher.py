7840067f8f5624846c6ba9531b4b522aad3c2588
Jérôme LECHAT <jlechat2000@icloud.com>
2025-08-07 22:49:50 +0200

    VERSION 1.0 FOR SCRAPING TOOL

---
+++ Watcher.py
@@ -0,0 +1,52 @@
+import os
+import json
+import pandas as pd
+
+from UTILS.LOGmaker import logger
+
+from WEBSITES.CIPACwatcher import CIPACwatcher
+from WEBSITES.CLABOTSwatcher import CLABOTSwatcher
+from WEBSITES.FIXAMIwatcher import FIXAMIwatcher
+from WEBSITES.KLIUMwatcher import KLIUMwatcher
+
+from UTILS.EXCELsender import EXCELsender
+from UTILS.CSVmerger import FINALdf
+from UTILS.MAILsender import MAILsender
+
+Logger = logger("WATCHER")
+Logger.info("Démarrage de Watcher.py...")
+
+Logger.info("Démarrage de CIPACwatcher.py...")
+CIPACdf, CIPACxlsx = CIPACwatcher()
+
+Logger.info("Démarrage de CLABOTSwatcher.py...")
+CLABOTSdf, CLABOTSxlsx = CLABOTSwatcher()
+
+Logger.info("Démarrage de FIXAMIwatcher.py...")
+FIXAMIdf, FIXAMIxlsx = FIXAMIwatcher()
+
+Logger.info("Démarrage de KLIUMwatcher.py...")
+KLIUMdf, KLIUMxlsx = KLIUMwatcher()
+
+Logger.info("Démarrage de CSVmerger.py...")
+FINALcsv, FINALxlsx = FINALdf([CIPACdf, CLABOTSdf, FIXAMIdf, KLIUMdf])
+
+Logger.info("Sending results...")
+EXCELsender(FINALcsv)
+
+Logger.info("Sending report...")
+with open("CONFIGS/EMAILconfig.json", "r", encoding="utf-8") as f:
+    mail_config = json.load(f)
+body = "\n".join(mail_config["BodyLines"])
+
+MAILsender(
+    sender_email=mail_config["Source"],
+    password=mail_config["password"],
+    recipient_email=mail_config["Target"],
+    subject=mail_config["Subject"],
+    body=mail_config["Body"],
+    filename=FINALxlsx
+)
+
+Logger.info("Shutting down...")
+exit()
